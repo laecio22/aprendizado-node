0af365b22dd0c3e135cd66dec4cac0f7b0cba4ee
laecio22 <laecon22@hotmail.com>
2025-04-29 14:56:29 -0300

    feat:instalação  nodemon  e  criação  de  algumas  rotas

--- App.js
+++ App.js
@@ -2,6 +2,22 @@ const express =  require('express')
 
 const  app = express()
 
+app.get('/' , function (req, res){
+   res.send('Home  da  aplicação')
+})
+
+app.get('/animais', function(req,res){
+    res.send('Página de  listagem  de  animais')
+})
+
+app.get('/animais/:id', function(req,res){
+    res.send(`animal  de  id : ${req.params.id}`)
+})
+
+app.get('/dashboard', function(req, res){
+    res.send('Página  de dashboard')
+})
+
 app.listen(8081, function(){
     console.log('Servidor  funcionando')
 })
