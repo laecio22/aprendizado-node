ff39134eb4c57f9c81150ab6e032a000287d46f7
laecio22 <laecon22@hotmail.com>
2025-05-28 10:48:20 -0300

    refactor:mudança  pra  arrow  function nas  funções  da  api  e  criação  de novas  rotas  da  aplicação

--- App.js
+++ App.js
@@ -2,22 +2,39 @@ const express =  require('express')
 
 const  app = express()
 
-app.get('/' , function (req, res){
+app.get('/' ,  (req, res) =>{
    res.send('Home  da  aplicação')
 })
 
-app.get('/animais', function(req,res){
+app.get('/animais', (req,res ) =>{
     res.send('Página de  listagem  de  animais')
 })
 
-app.get('/animais/:id', function(req,res){
+app.get('/animais/:id', (req,res) => {
     res.send(`animal  de  id : ${req.params.id}`)
 })
 
-app.get('/dashboard', function(req, res){
+app.get('/dashboard', (req, res) => {
     res.send('Página  de dashboard')
 })
 
-app.listen(8081, function(){
+app.get('/eventos', (req, res)=>{
+   res.send('Página  de  Eventos')
+})
+
+app.get('/voluntarios', (req, res)=>{
+    res.send('Página  de voluntarios')
+})
+
+app.get('/adocoes', (req, res)=> {
+ res.send('Página  de  adoções')
+})
+
+app.get('/configuracoes', (req, res)=> {
+   res.send('Página  de configurações')
+})
+
+app.listen(8081, () =>{
+    console.log('Acessar  http://localhost:8081')
     console.log('Servidor  funcionando')
 })
