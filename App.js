2c9cc8fb573bd8b57e15d33bfb7b3739a3d9fe14
laecio22 <laecon22@hotmail.com>
2025-09-19 14:27:05 -0300

    feat:separação  das rotas  em  Controllers

--- App.js
+++ App.js
@@ -1,40 +1,45 @@
-const express =  require('express')
 
-const  app = express()
+const express = require("express");
 
-app.get('/' ,  (req, res) =>{
-   res.send('Home  da  aplicação')
-})
+const app = express();
+const routes = require("./routes")
 
-app.get('/animais', (req,res ) =>{
-    res.send('Página de  listagem  de  animais')
-})
+app.use(express.urlencoded({extended:true}))
+app.use(routes)
 
-app.get('/animais/:id', (req,res) => {
-    res.send(`animal  de  id : ${req.params.id}`)
-})
 
-app.get('/dashboard', (req, res) => {
-    res.send('Página  de dashboard')
-})
 
-app.get('/eventos', (req, res)=>{
-   res.send('Página  de  Eventos')
-})
+// app.get("/animais", (req, res) => {
+//   res.send("Página de  listagem  de  animais");
+// });
 
-app.get('/voluntarios', (req, res)=>{
-    res.send('Página  de voluntarios')
-})
+// app.get("/animais/:id", (req, res) => {
+//   console.log(req.params.id)
+//   console.log(req.query.nome)
+//   res.send(`animal  de  id : ${req.params.id}`);
+// });
 
-app.get('/adocoes', (req, res)=> {
- res.send('Página  de  adoções')
-})
+// app.get("/dashboard", (req, res) => {
+//   res.send("Página  de dashboard");
+// });
 
-app.get('/configuracoes', (req, res)=> {
-   res.send('Página  de configurações')
-})
+// app.get("/eventos", (req, res) => {
+//   res.send("Página  de  Eventos");
+// });
 
-app.listen(8081, () =>{
-    console.log('Acessar  http://localhost:8081')
-    console.log('Servidor  funcionando')
-})
+// app.get("/voluntarios", (req, res) => {
+//   res.send("Página  de voluntarios");
+// });
+
+// app.get("/adocoes", (req, res) => {
+//   res.send("Página  de  adoções");
+// });
+
+// app.get("/configuracoes", (req, res) => {
+//   res.send("Página  de configurações");
+// });
+
+app.listen(8081, () => {
+  console.log("Acessar  http://localhost:8081");
+  console.log("Servidor  funcionando");
+});
